b3309d73c8eca067829327a29f112e62aabd543c
davinbarron <davinbarron667@gmail.com>
2025-09-28 13:34:29 +0100

    Updating PlacemarkActivity.kt - removing boilerplate code and adding timber logging

--- app/src/main/java/org/wit/placemark/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemark/PlacemarkActivity.kt
@@ -1,20 +1,15 @@
 package org.wit.placemark
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import timber.log.Timber
 
 class PlacemarkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_placemark)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Timber.plant(Timber.DebugTree())
+        Timber.i("Placemark Activity started..")
     }
 }
