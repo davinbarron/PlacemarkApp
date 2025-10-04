8b164d4337ffe6e9990d99802ebb33aff524e9ad
davinbarron <davinbarron667@gmail.com>
2025-10-04 13:52:19 +0100

    Updating PlacemarkListActivity.kt

--- app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
@@ -1,21 +1,17 @@
 package org.wit.placemark.activities
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
 import org.wit.placemark.R
+import org.wit.placemark.main.MainApp
 
 class PlacemarkListActivity : AppCompatActivity() {
+
+    lateinit var app: MainApp
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_placemark_list)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        app = application as MainApp
     }
 }
